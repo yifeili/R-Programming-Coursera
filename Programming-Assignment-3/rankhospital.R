78ca381af456c7002b4411f8b3d5506fbbf3b667
Yifei Li <syfifi1988@hotmail.com>
2016-06-13 19:03:40 -0500

    Programming Assignment 3 for R

---
+++ Programming-Assignment-3/rankhospital.R
@@ -0,0 +1,49 @@
+rankhospital <- function (state, outcome, num = "best"){
+  data <- read.csv("outcome-of-care-measures.csv")
+  A <- data$State == state
+  ## Test if the variable: state is in the list of data$State
+  ## If not, the sum of A will be 0.
+  if (!sum(A)) {
+    stop ("invalid state")
+  }
+  
+  disease_list <- c("heart attack", "heart failure", "pneumonia")
+  ## Test if the variable: outcome is in the list of disease
+  if (!outcome %in% disease_list){
+    stop ("invalid outcome")
+  }
+  ## Create the sub-data.frame for the specific state.
+  StateData <- subset(data, State == state)
+  ## Extract the hospital and rate colume from the data.
+  if (outcome == "heart attack") { 
+    StateData <- StateData[c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
+  }
+  else if (outcome == "heart failure"){
+    StateData <- StateData[c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
+  }
+  else {
+    StateData <- StateData[c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
+  }
+  ## Assign a common colume name for the StateData whatever the disease is.
+  colnames(StateData) <- c("Hospital.Name", "Disease.Rate")
+  
+  ## Transform the disease.rate colume from Factor to numeric for the purpose of ordering.So does the hospital colume. 
+  StateData[, "Disease.Rate"] <- as.numeric(as.character(StateData[, "Disease.Rate"]))
+  StateData[, "Hospital.Name"] <- as.character(StateData[, "Hospital.Name"])
+  
+  ## Order the data.frame by disease rate and hospital names.
+  StateData <- StateData[order(StateData$Disease.Rate, StateData$Hospital.Name), ] 
+
+  ## Specify the exact value for num input.  
+  N <- sum(!is.na(StateData$Disease.Rate))
+  if (num == "best"){
+    num <- 1
+  }
+  else if (num == "worst"){
+    num <- N
+  }
+  else{}
+  
+  Hospital <- StateData[num, "Hospital.Name"]
+  Hospital
+}
